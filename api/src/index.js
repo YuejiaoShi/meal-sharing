3b3222bb4c6eac970a8ce29d1c9c897260e5fc56
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-05 13:48:28 +0200

    rewrite "/api" message

--- api/src/index.js
+++ api/src/index.js
@@ -2,8 +2,8 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import knex from "./database_client.js";
-import nestedRouter from "./routers/nested.js";
+// import knex from "./database_client.js";
+// import nestedRouter from "./routers/nested.js";
 import allMeals from "./routers/all-meals.js";
 import futureMeals from "./routers/future-meals.js";
 
@@ -13,20 +13,25 @@ app.use(bodyParser.json());
 
 const apiRouter = express.Router();
 
-// You can delete this route once you add your own routes
 apiRouter.get("/", async (req, res) => {
-  const SHOW_TABLES_QUERY =
-    process.env.DB_CLIENT === "pg"
-      ? "SELECT * FROM pg_catalog.pg_tables;"
-      : "SHOW TABLES;";
-  const tables = await knex.raw(SHOW_TABLES_QUERY);
-  res.json({ tables });
+  res.json({
+    hello: "Welcome to Yuejiao's Restaurant API!",
+    description: "Explore our meal offerings through the following routes:",
+    routes: {
+      "/api/future-meals": "Meals scheduled for the future.",
+      "/api/past-meals": "Meals that have already occurred.",
+      "/api/all-meals": "All meals sorted by ID.",
+      "/api/first-meal": "The meal with the smallest ID.",
+      "/api/last-meal": "The meal with the largest ID.",
+    },
+  });
 });
 
-// This nested router example can also be replaced with your own sub-router
-apiRouter.use("/nested", nestedRouter);
+// ******** Sub-Routers ********
+// apiRouter.use("/nested", nestedRouter);
 apiRouter.use("/all-meals", allMeals);
 apiRouter.use("/future-meals", futureMeals);
+// ******** Sub-Routers ********
 
 
 app.use("/api", apiRouter);
