e3aa0ee333a75e1a0811168fc0921a59a4cc064d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-27 22:29:28 +0200

    add 2 more state loading and error

--- frontend/components/MealsList.jsx
+++ frontend/components/MealsList.jsx
@@ -1,10 +1,28 @@
+"use client";
+import { useEffect, useState } from "react";
+
 function MealsList() {
   const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:3001/api/all-meals")
-      .then((res) => res.json())
-      .then((meals) => setMeals(meals));
+    const fetchMeals = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/api/all-meals");
+        if (!res.ok) {
+          throw new Error(`Failed to fetch meals. Status: ${res.status}`);
+        }
+        const data = await res.json();
+        setMeals(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMeals();
   }, []);
 
   return (
