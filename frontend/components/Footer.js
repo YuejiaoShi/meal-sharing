8fba69bd5b4b76277adf4b77ceb1b230438dbe92
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-28 15:42:20 +0200

    change color

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,18 +1,18 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-6">
+    <footer className="bg-lime-900 text-white py-6">
       <div className="container mx-auto text-center">
         <p className="text-sm">
           &copy; 2023 Meal Sharing. All rights reserved.
         </p>
         <div className="flex justify-center space-x-4 mt-4">
-          <a href="/meals" className="hover:text-gray-400">
+          <a href="/meals" className="hover:text-lime-300">
             Menu
           </a>
-          <a href="/about" className="hover:text-gray-400">
+          <a href="/about" className="hover:text-lime-300">
             About Us
           </a>
-          <a href="/contact" className="hover:text-gray-400">
+          <a href="/contact" className="hover:text-lime-300">
             Contact
           </a>
         </div>
