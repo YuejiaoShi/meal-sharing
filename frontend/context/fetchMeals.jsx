1215cbef44af84f5db607b231421b03b1ed0f403
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-28 15:02:04 +0200

    specify a .env.local file to define NEXT_PUBLIC_API_URL, replace all api to this

--- frontend/context/fetchMeals.jsx
+++ frontend/context/fetchMeals.jsx
@@ -1,7 +1,7 @@
 // fetch all meals from the API
 export const fetchMeals = async () => {
   try {
-    const res = await fetch("http://localhost:3001/api/all-meals");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/all-meals`);
     if (!res.ok) {
       throw new Error(`Failed to fetch meals. Status: ${res.status}`);
     }
