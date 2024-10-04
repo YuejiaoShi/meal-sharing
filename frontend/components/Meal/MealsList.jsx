b7e5cf3cda43f364c4f1d9d0537c681c401b8b5d
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-04 17:53:07 +0200

    add some styles

--- frontend/components/Meal/MealsList.jsx
+++ frontend/components/Meal/MealsList.jsx
@@ -27,10 +27,12 @@ function MealsList({ previewCount = 0 }) {
     previewCount > 0 ? meals.slice(0, previewCount) : meals;
 
   return (
-    <div>
-      {mealsToDisplay.map((meal) => (
-        <Meal meal={meal} key={meal.id} />
-      ))}
+    <div className="max-w-5xl mx-auto p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {mealsToDisplay.map((meal) => (
+          <Meal meal={meal} key={meal.id} />
+        ))}
+      </div>
     </div>
   );
 }
