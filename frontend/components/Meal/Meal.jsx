cfbb2c9629d9707c86fc4af9c09c45238d7eac14
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-04 17:53:43 +0200

    display the picture and add some css styles

--- frontend/components/Meal/Meal.jsx
+++ frontend/components/Meal/Meal.jsx
@@ -1,15 +1,36 @@
+import { useThemeContext } from "@/context/themeContext";
+
 function Meal({ meal }) {
+  const theme = useThemeContext();
   return (
-    <div>
-      <p>
-        <strong>Title:</strong> {meal.title}
-      </p>
-      <p>
-        <strong>Description:</strong> {meal.description}
-      </p>
-      <p>
-        <strong>Price:</strong> ${meal.price}
-      </p>
+    <div
+      className={`rounded-lg p-6 transition-shadow duration-200 ${
+        theme.isDarkMode
+          ? "bg-darkMode-bg text-darkMode-text hover:shadow-[0_0_10px_#9bebc9]"
+          : "bg-white text-lightMode-text shadow-md hover:shadow-lg"
+      }`}
+    >
+      <img
+        className="w-full h-48 object-cover rounded-t-lg"
+        src={meal.image}
+        alt={`image of ${meal.title}`}
+      />
+      <div className="mt-4">
+        <p className="text-lg font-semibold">
+          <strong>Title:</strong> {meal.title}
+        </p>
+        <p
+          className={`mt-2 ${
+            theme.isDarkMode ? "text-gray-300" : "text-gray-600"
+          }`}
+        >
+          <strong>Description:</strong> {meal.description}
+        </p>
+        <p className="mt-2 font-bold">
+          <strong>Price: </strong>
+          {meal.price} kr
+        </p>
+      </div>
     </div>
   );
 }
