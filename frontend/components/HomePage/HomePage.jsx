dfb8dbda4a18e147b8a6b165d69ae556f2a20037
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-10 22:28:04 +0200

    decorate Homepage

--- frontend/components/HomePage/HomePage.jsx
+++ frontend/components/HomePage/HomePage.jsx
@@ -4,12 +4,45 @@ import MealsList from "../Meal/MealsList";
 
 function HomePage() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <MealsList previewCount={3} />
-      <Link href="/meals" className="text-blue-700">
-        more meals...
-      </Link>
+    <div className="min-h-screen ">
+      <div
+        className="relative bg-cover bg-center h-96"
+        style={{ backgroundImage: "url('/HomePage/HomeHeading.jpg')" }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="text-center text-white">
+            <h1 className="text-5xl font-bold mb-4">
+              Discover Delicious Meals
+            </h1>
+            <p className="text-lg mb-6">
+              Freshly made meals, hand-picked just for you!
+            </p>
+            <Link
+              href="/meals"
+              className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full"
+            >
+              Explore Our Meals
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <section className="py-12">
+        <div className="container mx-auto">
+          <h2 className="text-3xl font-semibold text-center mb-8">
+            Our Top Picks for You
+          </h2>
+          <MealsList previewCount={3} />
+          <div className="text-center mt-6">
+            <Link
+              href="/meals"
+              className="bg-lightMode-bg text-lightMode-text hover:bg-lightMode-hover font-bold py-2 px-4 rounded-full"
+            >
+              more meals...
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
