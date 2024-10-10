5ed82e5a3f49336defd16efde2afc6532bf6d934
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-10 22:04:55 +0200

    add spacing

--- frontend/components/Meal/Meal.jsx
+++ frontend/components/Meal/Meal.jsx
@@ -6,7 +6,7 @@ function Meal({ meal }) {
   return (
     <Link href={`/meals/${meal.id}`}>
       <div
-        className={`flex flex-col justify-between rounded-lg h-full p-6 transition-shadow duration-200${
+        className={`flex flex-col justify-between rounded-lg h-full p-6 transition-shadow duration-200 ${
           theme.isDarkMode
             ? "bg-darkMode-bg text-darkMode-text hover:shadow-[0_0_10px_#9bebc9]"
             : "bg-white text-lightMode-text shadow-md hover:shadow-lg"
