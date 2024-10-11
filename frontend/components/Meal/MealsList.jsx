9df64a5fabc4a403005c255d9d08c20d29389d6e
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-12 00:22:22 +0200

    clear up the MealsList to just accept meals prop

--- frontend/components/Meal/MealsList.jsx
+++ frontend/components/Meal/MealsList.jsx
@@ -1,35 +1,11 @@
 "use client";
-import { fetchMeals } from "@/lib/fetchMeals";
-import { useEffect, useState } from "react";
 import Meal from "./Meal";
 
-function MealsList({ previewCount = 0 }) {
-  const [meals, setMeals] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const loadMeals = async () => {
-      try {
-        const mealsData = await fetchMeals();
-        setMeals(mealsData);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    loadMeals();
-  }, []);
-
-  const mealsToDisplay =
-    previewCount > 0 ? meals.slice(0, previewCount) : meals;
-
+function MealsList({ meals }) {
   return (
     <div className="max-w-5xl mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {mealsToDisplay.map((meal) => (
+        {meals.map((meal) => (
           <Meal meal={meal} key={meal.id} />
         ))}
       </div>
