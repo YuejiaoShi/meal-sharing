5e3b89e011805196890b2a7ab53e28e0a7a4a76b
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-12 23:23:52 +0200

    adjust some styles

--- frontend/app/meals/page.js
+++ frontend/app/meals/page.js
@@ -60,17 +60,6 @@ function MealsPage() {
   return (
     <div className="p-5">
       <div className="flex justify-center items-center max-w-5xl mx-auto p-4">
-        <button
-          type="button"
-          onClick={fetchAllMeals}
-          className={`mr-5 flex justify-center items-center h-10 min-w-max p-3 rounded-full ${
-            theme.isDarkMode
-              ? "bg-darkMode-bg text-darkMode-text hover:bg-darkMode-hover"
-              : "bg-lightMode-bg text-lightMode-text hover:bg-lightMode-hover"
-          } shadow-md`}
-        >
-          ✨ Show All Meals
-        </button>
         <SearchBar
           searchQuery={searchQuery}
           setSearchQuery={setSearchQuery}
@@ -78,24 +67,59 @@ function MealsPage() {
         />
       </div>
 
-      <div className="flex justify-center items-center mb-4">
-        <select
-          className="mr-2 p-2 border rounded"
-          value={sortKey}
-          onChange={(e) => setSortKey(e.target.value)}
-        >
-          <option value="when">Date</option>
-          <option value="price">Price</option>
-          <option value="max_reservations">Max Reservations</option>
-        </select>
-        <select
-          className="p-2 border rounded"
-          value={sortDir}
-          onChange={(e) => setSortDir(e.target.value)}
-        >
-          <option value="ASC">Ascending</option>
-          <option value="DESC">Descending</option>
-        </select>
+      <div className="max-w-5xl mx-auto flex justify-evenly items-center pt-2 flex-wrap">
+        <div className="flex items-center justify-evenly w-4/5 flex-wrap">
+          <button
+            type="button"
+            onClick={fetchAllMeals}
+            className={`flex justify-center items-center h-10 px-4 rounded-full ${
+              theme.isDarkMode
+                ? "bg-darkMode-bg text-darkMode-text hover:bg-darkMode-hover"
+                : "bg-lightMode-bg text-lightMode-text hover:bg-lightMode-hover"
+            } shadow-md transition duration-200 ease-in-out transform hover:scale-105 w-full max-w-44 mb-2`}
+          >
+            ✨ View All Meals
+          </button>
+
+          <div className="flex flex-col sm:flex-row mb-2 w-full max-w-[400px] items-center m-2">
+            <div className="flex items-center text-lightMode-text m-2">
+              <label
+                htmlFor="sortKey"
+                className="mr-1 text-lg italic w-16 text-right"
+              >
+                Sort by
+              </label>
+              <select
+                id="sortKey"
+                className="p-1 w-32 text-center border-b-2 border-lightMode-bg focus:outline-none focus:border-b-4 focus:font-bold"
+                value={sortKey}
+                onChange={(e) => setSortKey(e.target.value)}
+              >
+                <option value="when">Date</option>
+                <option value="price">Price</option>
+                <option value="max_reservations">Max Reservations</option>
+              </select>
+            </div>
+
+            <div className="flex items-center text-lightMode-text">
+              <label
+                htmlFor="sortDir"
+                className="mr-1 text-lg italic w-16 text-right"
+              >
+                Order
+              </label>
+              <select
+                id="sortDir"
+                className="p-1 w-32 text-center border-b-2 border-lightMode-bg focus:outline-none focus:border-b-4 focus:font-bold"
+                value={sortDir}
+                onChange={(e) => setSortDir(e.target.value)}
+              >
+                <option value="ASC">Ascending</option>
+                <option value="DESC">Descending</option>
+              </select>
+            </div>
+          </div>
+        </div>
       </div>
 
       {loading && (
