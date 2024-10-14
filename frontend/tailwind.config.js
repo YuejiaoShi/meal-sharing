7c138c1d9b65cbd0574f80b49cf26769ecd7da6e
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-14 23:35:57 +0200

    modify screens setting

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
       colors: {
         primary: "#0c4022",
         darkMode: {
