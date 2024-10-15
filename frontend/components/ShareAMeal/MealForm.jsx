9c99fef2c354d6454a66c4f606819bc830d07700
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-10-16 00:50:12 +0200

    adjust styles

--- frontend/components/ShareAMeal/MealForm.jsx
+++ frontend/components/ShareAMeal/MealForm.jsx
@@ -1,6 +1,6 @@
-// MealForm.js
+"use client";
 
-import { useState } from "react";
+import { useThemeContext } from "@/context/themeContext";
 
 const MealForm = ({
   mealData,
@@ -12,6 +12,38 @@ const MealForm = ({
   message,
   loading,
 }) => {
+  const theme = useThemeContext();
+
+  const fieldConfigs = [
+    { name: "title", type: "text", label: "Title", required: true },
+    {
+      name: "description",
+      type: "textarea",
+      label: "Description",
+      required: true,
+    },
+    { name: "location", type: "text", label: "Location", required: true },
+    {
+      name: "max_reservations",
+      type: "number",
+      label: "Max Reservations",
+      required: true,
+    },
+    {
+      name: "price",
+      type: "number",
+      label: "Price",
+      required: true,
+      step: "0.01",
+    },
+    {
+      name: "when",
+      type: "datetime-local",
+      label: "When (YYYY-MM-DD HH:MM:SS)",
+      required: true,
+    },
+  ];
+
   const handleChange = (e) => {
     const { name, value } = e.target;
 
@@ -32,27 +64,34 @@ const MealForm = ({
   const handleImageChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      // Validate file type
       if (!file.type.startsWith("image/")) {
         setError("Please upload a valid image file.");
         return;
       }
-      // Validate file size (e.g., max 5MB)
+
       if (file.size > 5 * 1024 * 1024) {
         setError("File size should be less than 5MB.");
         return;
       }
-      setSelectedImage(URL.createObjectURL(file)); // Use URL.createObjectURL for preview
-      setMealData({ ...mealData, image: file }); // Set the file directly
+      setSelectedImage(URL.createObjectURL(file));
+      setMealData({ ...mealData, image: file });
     }
   };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="max-w-lg mx-auto bg-white shadow-lg rounded-lg p-6"
+      className={`max-w-lg mx-auto p-6 rounded-lg shadow-xl ${
+        theme.isDarkMode ? "bg-darkMode-bg" : "white"
+      }`}
     >
-      <h2 className="text-center text-2xl font-bold mb-4">Share a Meal</h2>
+      <h2
+        className={`text-center text-2xl font-bold mb-4 ${
+          theme.isDarkMode ? "text-white" : "text-lightMode-text"
+        }`}
+      >
+        Share a Meal
+      </h2>
 
       {message && (
         <p className="bg-green-100 border border-green-500 text-green-700 px-4 py-2 rounded-md shadow-md mb-4">
@@ -65,113 +104,60 @@ const MealForm = ({
         </p>
       )}
 
-      <div className="mb-4">
-        <label htmlFor="title" className="block text-lg font-medium mb-1">
-          Title:
-        </label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          value={mealData.title}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label htmlFor="description" className="block text-lg font-medium mb-1">
-          Description:
-        </label>
-        <textarea
-          name="description"
-          id="description"
-          value={mealData.description}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        ></textarea>
-      </div>
-
-      <div className="mb-4">
-        <label htmlFor="location" className="block text-lg font-medium mb-1">
-          Location:
-        </label>
-        <input
-          type="text"
-          name="location"
-          id="location"
-          value={mealData.location}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label
-          htmlFor="max_reservations"
-          className="block text-lg font-medium mb-1"
-        >
-          Max Reservations:
-        </label>
-        <input
-          type="number"
-          name="max_reservations"
-          id="max_reservations"
-          value={mealData.max_reservations}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label htmlFor="price" className="block text-lg font-medium mb-1">
-          Price:
-        </label>
-        <input
-          type="number"
-          name="price"
-          id="price"
-          value={mealData.price}
-          onChange={handleChange}
-          step="0.01"
-          required
-          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label htmlFor="when" className="block text-lg font-medium mb-1">
-          When (YYYY-MM-DD HH:MM:SS):
-        </label>
-        <input
-          type="datetime-local"
-          name="when"
-          id="when"
-          value={mealData.when}
-          onChange={handleChange}
-          required
-          className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
+      {fieldConfigs.map((field) => (
+        <div className="mb-4" key={field.name}>
+          <label
+            htmlFor={field.name}
+            className={`block text-lg font-medium mb-1 ${
+              theme.isDarkMode ? "text-white" : "text-lightMode-text"
+            }`}
+          >
+            {field.label}:
+          </label>
+          {field.type === "textarea" ? (
+            <textarea
+              name={field.name}
+              id={field.name}
+              value={mealData[field.name]}
+              onChange={handleChange}
+              required={field.required}
+              className={`w-full px-3 py-2 border rounded-md ${
+                theme.isDarkMode ? "border-gray-700" : "border-lightMode-hover"
+              } focus:outline-none focus:ring-2 focus:ring-lightMode-hover transition duration-200`}
+            ></textarea>
+          ) : (
+            <input
+              type={field.type}
+              name={field.name}
+              id={field.name}
+              value={mealData[field.name]}
+              onChange={handleChange}
+              required={field.required}
+              step={field.step}
+              className={`w-full px-3 py-2 border rounded-md ${
+                theme.isDarkMode ? "border-gray-700" : "border-lightMode-hover"
+              } focus:outline-none focus:ring-2 focus:ring-lightMode-hover 
+              } transition duration-200`}
+            />
+          )}
+        </div>
+      ))}
 
       <div className="mb-4">
         <input
           type="file"
           accept="image/*"
           onChange={handleImageChange}
-          className="mb-4"
+          className={`mb-4 border rounded-md px-3 py-2 transition duration-200 focus:outline-none focus:ring-2 ${
+            theme.isDarkMode ? "border-white" : "border-lightMode-hover"
+          } focus:ring-primary`}
         />
         {selectedImage && (
           <div>
-            <p>Selected Image Preview:</p>
             <img
               src={selectedImage}
               alt="Selected"
-              className="w-48 h-48 object-cover mb-4 rounded-md border"
+              className="w-48 h-48 object-cover mb-4 rounded-md border border-lightMode-hover"
             />
           </div>
         )}
@@ -179,10 +165,12 @@ const MealForm = ({
 
       <button
         type="submit"
-        className={`w-full py-2 px-4 text-white rounded-md ${
+        className={`w-full py-2 px-4 rounded-md ${
           loading
-            ? "bg-gray-500 cursor-not-allowed"
-            : "bg-blue-600 hover:bg-blue-700"
+            ? "cursor-not-allowed"
+            : theme.isDarkMode
+            ? "bg-darkMode-darkBG hover:bg-darkMode-hover transition duration-200 text-darkMode-text"
+            : "bg-lightMode-bg hover:bg-lightMode-hover transition duration-200 text-lightMode-text"
         }`}
         disabled={loading}
       >
